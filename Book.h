178fd1c21d87a50ce0065776a5e9813ab3fc4fc4
Elon1971 <94976777+Elon1971@users.noreply.github.com>
2024-02-22 15:18:23 +0530

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,31 +1,43 @@
 #ifndef BOOK_H
 #define BOOK_H
 
-#include <iostream>
 #include <string>
-using namespace std;
 
 class Book {
 private:
-    string* author;
-    string* title;
+    std::string* author;
+    std::string* title;
     int* stock;
-    string* publisher;
+    std::string* publisher;
     double* price;
-
-public:
     int maxSize;
 
+public:
+    // Constructor and destructor
     Book(int maxSize = 10);
     ~Book();
 
-    void addBook(string author, string title, int stock, string publisher, double price);
+    // Function to add a book
+    void addBook(std::string author, std::string title, int stock, std::string publisher, double price);
+
+    // Function to check if the book is available
     bool isAvailable(int index) const;
+
+    // Function to get the price of the book
     double getPrice(int index) const;
+
+    // Function to sell copies of the book
     bool sellCopies(int index, int numCopies);
+
+    // Function to display book details
     void displayDetails(int index) const;
+
+    // Function to display all available books
     void displayAllBooks() const;
-    void buyBook(string author, string title, int numCopies);
+
+    // Function to buy a book
+    void buyBook(std::string author, std::string title, int numCopies);
 };
 
 #endif
+
